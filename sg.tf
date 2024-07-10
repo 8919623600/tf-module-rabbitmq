17b2bd6640aa8a87674e122fa5e4a5822a382767
Jyoti sahoo <jyoti29sahoo@gmail.com>
2024-07-10 13:32:47 +0530

    updated

--- sg.tf
+++ sg.tf
@@ -20,8 +20,8 @@ tags = {
     from_port       = "22"
     to_port         = "22"
     protocol        = "tcp"
-    # cidr_blocks     = [data.terraform_remote_state.vpc.outputs.VPC_ID, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
-    cidr_blocks     = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+    cidr_blocks     = [data.terraform_remote_state.vpc.outputs.VPC_ID, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+    # cidr_blocks     = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
 
   }
 
